b355a9f7de3d82560b8d08c8c2388c79e9593686
Tina Umlandt <tina.umlandt@gmail.com>
2016-12-07 18:37:15 +0100

    initial commit

---
+++ tools/deploy.js
@@ -0,0 +1,12 @@
+/**
+ * This configuration will suppress logging and sanitize error messages.
+ */
+var ghpages = require('gh-pages');
+var path = require('path');
+ghpages.publish(path.resolve(__dirname, '../build'), {
+  repo: 'https://' + process.env.GH_TOKEN +
+    '@github.com/jsunconf/2017.jsunconf.eu.git',
+  silent: true
+}, function(error) {
+  console.log(error);
+});
